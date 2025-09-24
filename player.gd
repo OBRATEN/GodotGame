934ddc0db5b83daa714cfaeea86aefa186bfa73b
Arthur <arturgaragula65@gmail.com>
2025-09-25 02:17:36 +0300

    player v1.1

--- player.gd
+++ player.gd
@@ -1,22 +1,30 @@
 extends CharacterBody2D
 
 const speed = 100 as float
+var current_dir = "none" as String
 
 func _physics_process(delta: float) -> void:
 	player_movement(delta)
 	
 func player_movement(delta: float):
 	if Input.is_action_pressed("ui_right"):
+		current_dir = "right"
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
+		current_dir = "left"
 		velocity.x = -speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_up"):
-		velocity.x = 0
-		velocity.y = speed
-	elif Input.is_action_pressed("ui_down"):
+		current_dir = "up"
 		velocity.x = 0
 		velocity.y = -speed
+	elif Input.is_action_pressed("ui_down"):
+		current_dir = "down"
+		velocity.x = 0
+		velocity.y = speed
+	else:
+		velocity.x = 0
+		velocity.y = 0
 	move_and_slide()
 	
