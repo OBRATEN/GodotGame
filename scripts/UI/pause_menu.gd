d699dd4fc8caadca8f91123ae0d9462f3f55c3b1
Maks0982 <maksimka.2005908@gmail.com>
2025-11-25 19:32:55 +0300

    UI V2

---
+++ scripts/UI/pause_menu.gd
@@ -0,0 +1,36 @@
+extends PanelContainer
+
+@export var pause_menu: PanelContainer
+
+func resume():
+	toggle_visibility(pause_menu)
+	get_tree().paused = false
+
+func pause():
+	toggle_visibility(pause_menu)
+	get_tree().paused = true
+
+func toggle_visibility(object):
+	if object.visible:
+		object.visible = false
+	else:
+		object.visible = true
+
+func esc():
+	if Input.is_action_just_pressed("Esc") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("Esc") and get_tree().paused == true:
+		resume()
+
+
+func _on_resume_pressed():
+	resume()
+
+func _on_call_menu_pressed():
+	pause()
+
+func _on_exit_pressed():
+	get_tree().quit()
+
+func _process(delta):
+	esc()
